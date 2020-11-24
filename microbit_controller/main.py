1ca4a0f1828885eac7ef51877f9cf95704da3915
ilies-bel <beldjilali.ilies@gmail.com>
2020-11-24 15:32:54 +0100

    db evol to influx + sensor display toggle

--- microbit_controller/main.py
+++ microbit_controller/main.py
@@ -2,13 +2,31 @@ from ssd1306 import initialize, clear_oled
 from ssd1306_text import add_text
 from microbit import *
 
-while true:
+Temp = "Temperature 12.5 C"
+Lum = "Luminosité 65"
+
+L1 = 0
+L2 = 2
+status = "TL"
+
+initialize(pinReset=pin0)
+clear_oled()
+
+
+while 1:
 
     if (button_a.is_pressed()) or (button_b.is_pressed()):
-        initialize(pinReset=pin0)
         clear_oled()
-        add_text(0, 0, "Salut !!!")
+        if (status == "TL"):
+            add_text(0, L1, Temp)
+            add_text(0, L2, Lum)
+            status = "LT"
+        else :
+            add_text(0, L2, Temp)
+            add_text(0, L1, Lum)
+            status = "TL"
 
-# initialize(pinReset=pin0)
-# clear_oled()
-# add_text(0, 0, "Salut !!!")
+        
+
+
+
