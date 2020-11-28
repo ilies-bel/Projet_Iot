ac27a5f21b4e92f41972c1f46a18e6fb4b2d2bd7
Gwendal-Code <60916470+Gwendal-Code@users.noreply.github.com>
2020-11-28 17:55:33 +0100

    Add files via upload

--- Microbit_controller/sensor_control/main.py
+++ Microbit_controller/sensor_control/main.py
@@ -8,22 +8,30 @@ status = "TL"
 
 initialize(pinReset=pin0)
 clear_oled()
-
+# Les deux fonctions suivantes permettent de récupérer
+# les valeurs de la température et de la luminosité.
+# Elles sont isolées car utilisées à plusieurs endroits du code.
+def recupval_t():
+    Temp = temperature()
+    text_Temp = "Temp = " + str(Temp)
+    return text_Temp
+def recupval_L():
+    Lum = display.read_light_level()
+    text_Lum = "Lum = " + str(Lum)
+    return text_Lum
 
 while True:
-    Temp = temperature()
-    textTemp = "Temp = " + str(Temp)
-    Lum = display.read_light_level()
-    textLum = "Lum = " + str(Lum)
+    txt_Temp = recupval_t()
+    txt_Lum = recupval_L()
 
     if (button_a.is_pressed()) or (button_b.is_pressed()):
         clear_oled()
 
         if (status == "TL"):
-            add_text(0, L1, textTemp)
-            add_text(0, L2, textLum)
+            add_text(0, L1, txt_Temp)
+            add_text(0, L2, txt_Lum)
             status = "LT"
         else :
-            add_text(0, L2, textTemp)
-            add_text(0, L1, textLum)
+            add_text(0, L2, txt_Temp)
+            add_text(0, L1, txt_Lum)
             status = "TL"
